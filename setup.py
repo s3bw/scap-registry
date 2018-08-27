a481d37d68a513b1bd3ad212f85d1641d458ebcc
Sebastien Williams-Wynn <sebastien@cytora.com>
2018-08-27 22:49:29 +0100

    CI: Add CI with tox and travis (#13)
        This has also forced me to specify
        my dependencies.
    
        Resolves: #2

--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setup(
             'scap_lib',
             'scap_registry',
         ]
-    )
+    ),
+    install_requires=[
+        'flask==0.12.2',
+        'requests==2.19.1',
+        'PyYAML==3.13',
+    ]
 )
