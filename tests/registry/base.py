1033d2e4a1222a51c5de06bacce61b8d4cc450f8
GiantsLoveDeathMetal <sebastien@cytora.com>
2019-01-11 00:33:21 +0000

    Add tests to get meta data endpoint

--- tests/registry/base.py
+++ tests/registry/base.py
@@ -2,20 +2,31 @@ import os
 import random
 import string
 import unittest
+import shutil
 
 
 os.environ['APP_ENV'] = 'test'
-
 
 import scap_registry  # noqa: E402
 
 
 class TestCase(unittest.TestCase):
+    #: allocate variable for a temp folder
+    tmp_folder = None
 
     def setUp(self):
         scap_registry.app.testing = True
         self.app = scap_registry.app.test_client()
 
     def generate_note_id(self, length=8):
-        return ''.join([random.choice(string.ascii_uppercase + string.digits)
-                        for x in range(length)])
+        """Generate note id and save it as temp folder name."""
+        self.tmp_folder = ''.join([
+            random.choice(string.ascii_uppercase + string.digits)
+            for x in range(length)
+        ])
+        return self.tmp_folder
+
+    def tearDown(self):
+        # If temp folder has been created, remove it
+        if self.tmp_folder:
+            shutil.rmtree('/tmp/test/notes/' + self.tmp_folder)
--- tests/registry/test_notes.py
+++ tests/registry/test_notes.py
@@ -5,8 +5,12 @@ import base
 
 
 class TestNotes(base.TestCase):
+    """Notes and content are first PUT
+        then we GET.
+    """
 
     def test_note_content(self):
+        """Test note content GET and PUT."""
         note_id = self.generate_note_id()
         endpoint = "/v1/notes/{}/content".format(note_id)
         text = ("My test note content"
@@ -31,6 +35,21 @@ class TestNotes(base.TestCase):
         resp = self.app.get(endpoint)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(json.loads(resp.data.decode()), json_data)
+
+    def test_get_meta_data(self):
+        note_id = self.generate_note_id()
+        json_data = json.dumps({'id': note_id})
+        endpoint = "/v1/notes/{}/json".format(note_id)
+        result = self.app.put(endpoint, data=json_data)
+        self.assertEqual(result.status_code, 200)
+
+        # Test meta_data endpoint
+        end_point = "/v1/meta_data/load"
+        result = self.app.get(end_point)
+        self.assertEqual(result.status_code, 200)
+
+        expected_data = {note_id: {'id': note_id}}
+        self.assertEqual(json.loads(result.data.decode()), expected_data)
 
     def test_note_not_found(self):
         resp = self.app.get('/v1/notes/NOTFOUND/content')
